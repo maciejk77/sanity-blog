7f90e82225a4f83be4a86e94f1cb402fcd113724
Maciej Kurek <maciejk77@gmail.com>
2024-04-25 01:24:25 +0100

    complete website

---
+++ schemaTypes/post.ts
@@ -0,0 +1,48 @@
+import {title} from 'process'
+
+export default {
+  name: 'post',
+  type: 'document',
+  title: 'Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'overview',
+      type: 'string',
+      title: 'Overview',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+        {
+          type: 'image',
+          fields: [
+            {
+              type: 'image',
+              name: 'alt',
+              title: 'Alternative text',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
